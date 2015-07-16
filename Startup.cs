fc1ca813b209cf3d7406a1a45acb643a736dc941
Travis K <traviskean04@live.com>
2015-07-16 11:19:07 -0400

    Created MVC Program

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(First_MVC_Program.Startup))]
+namespace First_MVC_Program
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
