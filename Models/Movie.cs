a18c4a182886017dceb781ec69bc0f85c587b162
Travis K <traviskean04@live.com>
2015-07-22 15:10:46 -0400

    Added Data Validation

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,15 +6,42 @@ namespace First_MVC_Program.Models
 {
     public class Movie
     {
+        public int ID { get; set; }
+
+
+
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+
+
+
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
-               
-        public int ID { get; set; }
-        public string Title { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+
+
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+
+
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+
+
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(20)]
         public string Rating { get; set; }
     }
      public class MovieDBContext : DbContext
