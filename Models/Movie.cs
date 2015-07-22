783f6576451cd5ae493b8abc647a546277c67389
Travis K <traviskean04@live.com>
2015-07-22 12:07:21 -0400

    Added Migrations and Seeding

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -9,12 +9,13 @@ namespace First_MVC_Program.Models
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
-
+               
         public int ID { get; set; }
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
      public class MovieDBContext : DbContext
     {
